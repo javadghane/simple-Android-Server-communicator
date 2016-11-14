86d9dfa5bcacaae6751f8c1a3f5570b7bb76bc80
Javad ghane Al-hoseiny <hi@javadroid.ir>
2016-11-15 02:15:29 +0330

    add Main AsyncFile

---
+++ AsyncPostData.java
@@ -0,0 +1,77 @@
+
+import android.os.AsyncTask;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by JavaDroid.ir on 4/17/2015.
+ */
+
+
+public class AsyncPostData extends AsyncTask<HashMap, Void, String> {
+
+
+    private String link = "";
+    String response = "";
+    public AsyncResponse delegate = null;
+
+
+    public static interface AsyncResponse {
+        void processFinish(String output);
+    }
+
+    public AsyncPostData(
+            String link, AsyncResponse list) {
+        this.link = link;
+        this.delegate = list;
+    }
+
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected String doInBackground(HashMap... maps) {
+        OkHttpClient client = new OkHttpClient();
+
+        FormEncodingBuilder form = new FormEncodingBuilder();
+        Set set = maps[0].entrySet();
+        Iterator i = set.iterator();
+        while (i.hasNext()) {
+            Map.Entry me = (Map.Entry) i.next();
+            form.add(me.getKey().toString(), me.getValue().toString());
+        }
+        RequestBody formBody = form.build();
+
+        Request request = new Request.Builder()
+                .url(this.link)
+                .post(formBody)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            response = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        if (delegate != null)
+            delegate.processFinish(result);
+
+    }
+
+}
